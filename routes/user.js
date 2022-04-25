a69e23311d985c67737ffdb76c87e3bf91b630bf
AnthonyCalandr <AnthonyCalandr@gmail.com>
2022-04-25 18:40:12 -0500

    Demo

--- routes/user.js
+++ routes/user.js
@@ -10,15 +10,133 @@ router.get('/', checkAuthenticated, (req, res) => {
 })
 
 router.get('/profile', checkAuthenticated, (req,res) => {
-    res.render('profile.ejs')
+    if(req.user.userDetails.length> 0){
+        res.render('profile.ejs', {message: "Resubmit to update your information"})
+    } else{
+    res.render('profile.ejs', {message: ""})
+    }
 })
 
+//get route for displaying edit profile and users profile page
+router.get('/userProfile', checkAuthenticated, (req, res) => {
+
+    console.log(req.user)
+
+    if(req.user.userDetails.length > 0){
+        const{name, address1, address2, city, zipcode, state} = req.user.userDetails[0]
+
+        let userInfo = {
+            name: name,
+            address1: address1,
+            address2: address2,
+            city: city,
+            zipcode: zipcode,
+            state: state
+        }
+
+    res.render('userProfile', {info: userInfo})
+    } else{
+        res.render('profile', {message: ""})
+    }
+})
+
+//post route for saving clients profile information
+router.post('/clientProfile', checkAuthenticated, (req, res) => {
+
+    const {name, address1, address2, city, zipcode, state} = req.body
+
+    //backend validation for client profile information 
+    if(name.length > 50){
+        res.render('profile', {message: "Name is required to be less than 50 characters"})
+    }
+
+    if(address1.length > 100){
+        res.render('profile', {message: "Street Address 1 length is required to be less than 100 characters"})
+    }
+
+    if(address2.length > 100){
+        res.render('profile', {message: "Street Address 2 length is required to be less than 100 characters"})
+    }
+
+    if(city.length > 100){
+        res.render('profile', {message: "City length is required to be less than 100 characters"})
+    }
+
+    if(zipcode.length < 5){
+        res.render('profile', {message: "Zipcode is required to be at least 5 characters"})
+    } else if(zipcode.length > 9){
+        res.render('profile', {message: "Zipcode is required to be less than 10 characters"})
+    } else {
+
+        let clientInfo = {
+            name: name,
+            address1: address1,
+            address2: address2,
+            city: city,
+            zipcode: zipcode,
+            state: state
+        }
+
+        let info = []
+        info.push(clientInfo)
+
+        let username = req.user.username
+
+        users.findOneAndUpdate({username: username}, {userDetails: info}, ()=> {
+            console.log("Client information submitted")
+        })
+
+        res.redirect('/user/userProfile')
+    }
+
+})
+
+//get route for displaying the fuel quote form page
 router.get('/fuelQuoteForm', checkAuthenticated, (req,res) => {
-    res.render('fuelQuoteForm.ejs')
+    if(req.user.userDetails.length > 0){
+
+    let address = req.user.userDetails[0].address
+    let state = req.user.userDetails[0].state
+    let quote = req.user.fuelQuotes.length
+
+    res.render('fuelQuoteForm', {address: address, quote: quote, state: state, message: ""})
+    } else{
+        res.render('profile', {message: "Please finish setting up your profile to gain access to fuel quote"})
+    }
 })
 
+//get route for displaying the fuel quote history page
 router.get('/fuelQuoteHistory', checkAuthenticated, (req,res) => {
-    res.render('fuelQuoteHistory.ejs')
+    if(req.user.userDetails.length > 0){
+    let fuelHistory = req.user.fuelQuotes
+    let date = req.user.userDetails[0].address
+
+    res.render('fuelQuoteHistory', {fuelHistory, deliver: date})
+    } else {
+        res.render('profile', {message: "Please finish setting up your profile to gain access to your fuel quote history"})
+    }
+})
+
+//post route for saving the clients fuel quote information
+router.post('/fuelQuoteInfo', checkAuthenticated, (req, res) =>{
+    let username = req.user.username
+    const {gallons, address, date, price, total} = req.body
+    let fQuote = req.user.fuelQuotes
+
+    let quoteInfo = {
+        gallons: gallons,
+        address: address,
+        date: date,
+        price: price,
+        total: total
+    }
+
+    fQuote.push(quoteInfo)
+
+    users.findOneAndUpdate({username: username}, {fuelQuotes: fQuote}, () =>{
+        console.log("Fuel Quote submitted")
+    })
+    res.redirect('/user/fuelQuoteHistory')
 })
 
 //route for logging the user out
